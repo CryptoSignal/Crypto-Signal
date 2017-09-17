6abc74bc9f62bcef24cca2971b5ea1042101e12d
AbenezerMamo <ElitePowered@gmail.com>
2017-09-17 12:28:40 -0700

    initial setup

---
+++ app.py
@@ -0,0 +1,35 @@
+from bittrex import Bittrex
+import json
+
+with open("secrets.json") as secrets_file:
+    secrets = json.load(secrets_file)
+    secrets_file.close()
+    my_bittrex = Bittrex(secrets['key'], secrets['secret'])
+
+# Let's test an API call to get our BTC balance as a test
+# print(my_bittrex.get_balance('BTC')['result']['Balance'])
+
+coin_pairs = ['BTC-ETH', 'BTC-OMG', 'BTC-GNT', 'BTC-CVC']
+
+
+#print(historical_data = my_bittrex.getHistoricalData('BTC-ETH', 30, "thirtyMin"))
+
+
+def calculateSMA(coin_pair, period, unit):
+    historical_data = my_bittrex.getHistoricalData(coin_pair, period, unit)
+    return historical_data
+
+def calculateEMA(coin_pair, period, unit):
+    historical_data = my_bittrex.getHistoricalData(coin_pair, period, unit)
+    return historical_data
+
+def calculateRSI(coin_pair, period, unit):
+    historical_data = my_bittrex.getHistoricalData(coin_pair, period, unit)
+    return historical_data
+
+def findBreakout(coin_pair, period, unit):
+    historical_data = my_bittrex.getHistoricalData(coin_pair, period, unit)
+    return historical_data
+
+
+print(findBreakout(coin_pair='BTC-ETH', period=30, unit="thirtyMin"))
