953b8b9821dd08d2aa0eb821b02cfb4043c83415
AbenezerMamo <ElitePowered@gmail.com>
2017-09-20 11:00:35 -0700

    added OS alerts when coins break out

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from bittrex import Bittrex
 import json
+import time
+import os
+import rumps
 
+rumps.debug_mode(True)
 with open("secrets.json") as secrets_file:
     secrets = json.load(secrets_file)
     secrets_file.close()
-    my_bittrex = Bittrex(secrets['key'], secrets['secret'])
+    my_bittrex = Bittrex(secrets['bittrex_key'], secrets['bittrex_secret'])
 
 # Let's test an API call to get our BTC balance as a test
 # print(my_bittrex.get_balance('BTC')['result']['Balance'])
 
 coin_pairs = ['BTC-ETH', 'BTC-OMG', 'BTC-GNT', 'BTC-CVC', 'BTC-BAT', 'BTC-XEL', 'BTC-FUN', 'BTC-STRAT', 'BTC-LSK']
-
 
 #print(historical_data = my_bittrex.getHistoricalData('BTC-ETH', 30, "thirtyMin"))
 def getClosingPrices(coin_pair, period, unit):
@@ -42,9 +45,16 @@ def findBreakout(coin_pair, period, unit):
             hit += 1
 
     if (hit / period) >= .75:
-        return "{} is breaking out!".format(coin_pair)
+        rumps.alert("BREAKING OUT!!!")
+        return "{}: BREAKING OUT!!!".format(coin_pair)
     else:
         return "{}: #Bagholding".format(coin_pair)
 
-for i in coin_pairs:
-    print(findBreakout(coin_pair=i, period=5, unit="oneMin"))
+
+if __name__ == "__main__":
+    def loop_script():
+        for i in coin_pairs:
+            print(findBreakout(coin_pair=i, period=5, unit="fiveMin"))
+        time.sleep(60)
+        loop_script()
+    loop_script()
