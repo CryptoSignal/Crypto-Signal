bbfbd3036046c76b5e55a295d15328c45b5c8d36
shads2 <shadow_reaver2@hotmail.com>
2017-12-30 19:13:20 +1100

    Fix for incorrect key

--- app/app.py
+++ app/app.py
@@ -183,8 +183,8 @@ if __name__ == "__main__":
     CONFIG['notifiers']['twilio']['required']['sender_number'] = os.environ.get('TWILIO_SENDER_NUMBER', CONFIG['notifiers']['twilio']['required']['sender_number'])
     CONFIG['notifiers']['twilio']['required']['receiver_number'] = os.environ.get('TWILIO_RECEIVER_NUMBER', CONFIG['notifiers']['twilio']['required']['receiver_number'])
     CONFIG['notifiers']['gmail']['required']['username'] = os.environ.get('GMAIL_USERNAME', CONFIG['notifiers']['gmail']['required']['username'])
-    CONFIG['notifiers']['gamil']['required']['password'] = os.environ.get('GMAIL_PASSWORD', CONFIG['notifiers']['gmail']['required']['password'])
-    CONFIG['notifiers']['gamil']['required']['destination_emails'] = os.environ.get('GMAIL_DESTINATION_EMAILS', CONFIG['notifiers']['gmail']['required']['destination_emails'])
+    CONFIG['notifiers']['gmail']['required']['password'] = os.environ.get('GMAIL_PASSWORD', CONFIG['notifiers']['gmail']['required']['password'])
+    CONFIG['notifiers']['gmail']['required']['destination_emails'] = os.environ.get('GMAIL_DESTINATION_EMAILS', CONFIG['notifiers']['gmail']['required']['destination_emails'])
 
     # Set up logger
     LOGGER = logging.getLogger(__name__)
