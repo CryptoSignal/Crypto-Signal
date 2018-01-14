c845000b1a20e08a54be88988a94bfbd1633cae5
Jesse Bartola <jrbartola@gmail.com>
2018-01-14 13:35:11 -0500

    Backtester class implementation

--- app/backtesting/backtest.py
+++ app/backtesting/backtest.py
@@ -6,11 +6,6 @@ from conf import Configuration
 from backtesting.chart import Chart
 from backtesting.strategy import BacktestingStrategy
 
-import json
-
-coins = ["ETH/BTC", "LTC/BTC", "XMR/BTC", "OMG/BTC", "XRP/BTC", "SC/BTC", "XEM/BTC", "DASH/BTC", "LSK/BTC",
-         "GNT/BTC", "VTC/BTC", "ETC/BTC", "STRAT/BTC", "DGB/BTC"]
-
 # Load settings and create the config object
 config = Configuration()
 settings = config.fetch_settings()
@@ -18,9 +13,36 @@ settings = config.fetch_settings()
 # Set up logger
 configure_logging(settings['loglevel'], settings['log_mode'])
 
-def main(coin):
+# Sample coin pairs to backtest with
+coins = ["ETH/BTC", "LTC/BTC", "XMR/BTC", "OMG/BTC", "XRP/BTC", "SC/BTC", "XEM/BTC", "DASH/BTC", "LSK/BTC",
+         "GNT/BTC", "VTC/BTC", "ETC/BTC", "STRAT/BTC", "DGB/BTC"]
 
-    chart = Chart(coin, "1h", "bittrex", config.fetch_exchange_config())
+
+class Backtester(object):
+    def __init__(self, coin_pair, period_length, exchange, capital, stop_loss, num_data_points, buy_strategy, sell_strategy, indicators={}):
+
+        self.chart = Chart(coin_pair, period_length, exchange, config.fetch_exchange_config(), length=num_data_points)
+
+        self.strategy = BacktestingStrategy(pair=coin_pair, capital=capital, buy_strategy=buy_strategy,
+                                       sell_strategy=sell_strategy, trading_fee=0.0025, stop_loss=stop_loss)
+
+        self.indicators = indicators
+
+    def run(self):
+        candlesticks = self.chart.get_points()
+        self.strategy.run(candlesticks)
+
+    def get_results(self):
+        closings = [[i, d.close] for i, d in enumerate(self.chart.get_points())]
+        indicators = self.chart.get_indicators(**self.indicators)
+
+        results = {'buys': list(self.strategy.buys), 'sells': list(self.strategy.sells), 'closingPrices': closings,
+                  'indicators': indicators, 'profit': round(self.strategy.profit, 8)}
+
+        return results
+
+
+def main(coin):
 
     buy_strategy = {'currentprice': {
         'comparator': 'GT',
@@ -32,11 +54,10 @@ def main(coin):
         'value': 'movingaverage9'
     }}
 
-    strategy = BacktestingStrategy(capital=1.0, pair=coin, trading_fee=0.0025, buy_strategy=buy_strategy,
-                                   sell_strategy=sell_strategy, stop_loss=0.00001)
+    backtester = Backtester(coin, "1h", "bittrex", capital=1.0, stop_loss=0.0001, buy_strategy=buy_strategy,
+                            sell_strategy=sell_strategy)
 
-    candlesticks = chart.get_points()
-    strategy.run(candlesticks)
+    backtester.run()
 
     # chart.plot_indicators(bollinger=21, movingaverage=[9, 15])
     # chart.plot_trades(strategy.buys, strategy.sells)
@@ -45,26 +66,8 @@ def main(coin):
     # closings = [[i, d.close] for i, d in enumerate(chart.get_points())]
     # indicators = chart.get_indicators(bollinger=21, movingaverage=[9, 15])
 
-    print("Total Profit (" + coin + "): " + str(strategy.profit))
+    print("Total Profit (" + coin + "): " + str(backtester.strategy.profit))
 
-
-def backtest(coin_pair, period_length, capital, stop_loss, num_data_points, buy_strategy, sell_strategy, indicators={}):
-
-    chart = Chart(coin_pair, period_length, "bittrex", config.fetch_exchange_config(), length=num_data_points)
-
-    strategy = BacktestingStrategy(pair=coin_pair, capital=capital, buy_strategy=buy_strategy,
-                                   sell_strategy=sell_strategy, trading_fee=0.0025, stop_loss=stop_loss)
-
-    candlesticks = chart.get_points()
-    strategy.run(candlesticks)
-
-    closings = [[i, d.close] for i, d in enumerate(candlesticks)]
-    indicators = chart.get_indicators(**indicators)
-
-    result = {'buys': list(strategy.buys), 'sells': list(strategy.sells), 'closingPrices': closings,
-              'indicators': indicators, 'profit': round(strategy.profit, 8)}
-
-    return result
 
 if __name__ == "__main__":
     for coin in coins:
