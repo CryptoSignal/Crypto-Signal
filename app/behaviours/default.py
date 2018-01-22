8e7882e276aa843d7a4170e55c012d3420991bef
shads2 <shadow_reaver2@hotmail.com>
2018-01-22 17:40:03 +1100

    features for default behaviour

--- app/behaviours/default.py
+++ app/behaviours/default.py
@@ -18,11 +18,12 @@ class DefaultBehaviour():
             exchange_interface (ExchangeInterface): Instance of the ExchangeInterface class for
                 making exchange queries.
             strategy_analyzer (StrategyAnalyzer): Instance of the StrategyAnalyzer class for
-                running analysis on exchange information.
+                running analyzed_data on exchange information.
             notifier (Notifier): Instance of the notifier class for informing a user when a
                 threshold has been crossed.
         """
 
+        self.logger = structlog.get_logger()
         self.behaviour_config = behaviour_config
         self.exchange_interface = exchange_interface
         self.strategy_analyzer = strategy_analyzer
@@ -34,10 +35,13 @@ class DefaultBehaviour():
         Args:
             market_pairs (list): List of symbol pairs to operate on, if empty get all pairs.
         """
+        self.logger.info("Starting default behaviour...")
 
         if market_pairs:
+            self.logger.debug("Found configured symbol pairs.")
             market_data = self.exchange_interface.get_symbol_markets(market_pairs)
         else:
+            self.logger.debug("No configured symbol pairs, using all available on exchange.")
             market_data = self.exchange_interface.get_exchange_markets()
 
         self.__test_strategies(market_data)
@@ -65,76 +69,72 @@ class DefaultBehaviour():
                         '5m'
                     )
 
-                    rsi_data = self.strategy_analyzer.analyze_rsi(
-                        one_day_historical_data,
-                        hot_thresh=self.behaviour_config['rsi']['hot'],
-                        cold_thresh=self.behaviour_config['rsi']['cold']
-                    )
+                    analyzed_data = {}
 
-                    sma_data = self.strategy_analyzer.analyze_sma(
-                        one_day_historical_data,
-                        hot_thresh=self.behaviour_config['sma']['hot'],
-                        cold_thresh=self.behaviour_config['sma']['cold']
-                    )
+                    if self.behaviour_config['rsi']['enabled']:
+                        analyzed_data['RSI'] = self.strategy_analyzer.analyze_rsi(
+                            one_day_historical_data,
+                            hot_thresh=self.behaviour_config['rsi']['hot'],
+                            cold_thresh=self.behaviour_config['rsi']['cold']
+                        )
 
-                    ema_data = self.strategy_analyzer.analyze_ema(
-                        one_day_historical_data,
-                        hot_thresh=self.behaviour_config['ema']['hot'],
-                        cold_thresh=self.behaviour_config['ema']['cold']
-                    )
+                    if self.behaviour_config['sma']['enabled']:
+                        analyzed_data['SMA'] = self.strategy_analyzer.analyze_sma(
+                            one_day_historical_data,
+                            hot_thresh=self.behaviour_config['sma']['hot'],
+                            cold_thresh=self.behaviour_config['sma']['cold']
+                        )
 
-                    breakout_data = self.strategy_analyzer.analyze_breakout(
-                        five_minute_historical_data,
-                        hot_thresh=self.behaviour_config['breakout']['hot'],
-                        cold_thresh=self.behaviour_config['breakout']['cold']
-                    )
+                    if self.behaviour_config['ema']['enabled']:
+                        analyzed_data['EMA'] = self.strategy_analyzer.analyze_ema(
+                            one_day_historical_data,
+                            hot_thresh=self.behaviour_config['ema']['hot'],
+                            cold_thresh=self.behaviour_config['ema']['cold']
+                        )
 
-                    ichimoku_data = self.strategy_analyzer.analyze_ichimoku_cloud(
-                        one_day_historical_data,
-                        hot_thresh=self.behaviour_config['ichimoku']['hot'],
-                        cold_thresh=self.behaviour_config['ichimoku']['cold']
-                    )
+                    if self.behaviour_config['breakout']['enabled']:
+                        analyzed_data['Breakout'] = self.strategy_analyzer.analyze_breakout(
+                            five_minute_historical_data,
+                            hot_thresh=self.behaviour_config['breakout']['hot'],
+                            cold_thresh=self.behaviour_config['breakout']['cold']
+                        )
 
-                    macd_data = self.strategy_analyzer.analyze_macd(
-                        one_day_historical_data,
-                        hot_thresh=self.behaviour_config['macd']['hot'],
-                        cold_thresh=self.behaviour_config['macd']['cold']
-                    )
+                    if self.behaviour_config['ichimoku']['enabled']:
+                        analyzed_data['Ichimoku'] = self.strategy_analyzer.analyze_ichimoku_cloud(
+                            one_day_historical_data,
+                            hot_thresh=self.behaviour_config['ichimoku']['hot'],
+                            cold_thresh=self.behaviour_config['ichimoku']['cold']
+                        )
+
+                    if self.behaviour_config['macd']['enabled']:
+                        analyzed_data['MACD'] = self.strategy_analyzer.analyze_macd(
+                            one_day_historical_data,
+                            hot_thresh=self.behaviour_config['macd']['hot'],
+                            cold_thresh=self.behaviour_config['macd']['cold']
+                        )
 
                 except ccxt.errors.RequestTimeout:
                     continue
 
                 message = ""
-                if breakout_data['is_hot']:
-                    message += "Breakout: {} is breaking out!\n".format(market_pair)
+                output = "{}: ".format(market_pair)
+                for analysis in analyzed_data:
+                    if self.behaviour_config[analysis.lower()]['alert_enabled']:
+                        if analyzed_data[analysis]['is_hot']:
+                            message += "{}: {} is hot!\n".format(analysis, market_pair)
 
-                if rsi_data['is_cold']:
-                    message += "RSI: {} is over bought!\n".format(market_pair)
-                elif rsi_data['is_hot']:
-                    message += "RSI: {} is over sold!\n".format(market_pair)
+                        if analyzed_data[analysis]['is_cold']:
+                            message += "{}: {} is cold!\n".format(analysis, market_pair)
 
-                if macd_data['is_hot']:
-                    message += "MACD: {} trend is good according to MACD!\n".format(market_pair)
-                if macd_data['is_cold']:
-                    message += "MACD: {} trend is poor according to MACD!\n".format(market_pair)
+                    formatted_values = []
+                    for value in analyzed_data[analysis]['values']:
+                        if isinstance(value, float):
+                            formatted_values.append(format(value, '.8f'))
+                        else:
+                            formatted_values.append(value)
 
-                if sma_data['is_hot']:
-                    message += "SMA: {} is trending well according to SMA!\n".format(market_pair)
-
-                if ema_data['is_hot']:
-                    message += "EMA: {} is trending well according to EMA!\n".format(market_pair)
-
-                if ichimoku_data['is_hot']:
-                    message += "IMC: {} is trending well according to Ichimoku!\n".format(market_pair)
+                    formatted_string = '/'.join(formatted_values)
+                    output += "\t{}: {} ".format(analysis, formatted_string)
 
                 if message:
                     self.notifier.notify_all(message)
-
-                print("{}: \tBreakout: {} \tRSI: {} \tSMA: {} \tEMA: {} \tIMC: {} \tMACD: {}".format(
-                    market_pair,
-                    breakout_data['values'][0],
-                    format(rsi_data['values'][0], '.2f'),
-                    format(sma_data['values'][0], '.7f'),
-                    format(ema_data['values'][0], '.7f'),
-                    format(ichimoku_data['values'][0], '.7f') + "/" + format(ichimoku_data['values'][1], '.7f'),
-                    format(macd_data['values'][0], '.7f')))
