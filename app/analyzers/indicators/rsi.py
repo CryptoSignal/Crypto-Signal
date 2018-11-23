eb48c7c5730fa2016cb03ab9e71ec2e41b47969f
Eduardo Zarate <eduardo@bbfour.com>
2018-11-23 10:36:41 -0600

    Changes on RSI indicator to use LRSI filter.

--- app/analyzers/indicators/rsi.py
+++ app/analyzers/indicators/rsi.py
@@ -7,11 +7,11 @@ import pandas
 from talib import abstract
 
 from analyzers.utils import IndicatorUtils
-
+from analyzers.informants.lrsi import LRSI
 
 class RSI(IndicatorUtils):
     def analyze(self, historical_data, period_count=14,
-                signal=['rsi'], hot_thresh=None, cold_thresh=None):
+                signal=['rsi'], hot_thresh=None, cold_thresh=None,  lrsi_filter=None):
         """Performs an RSI analysis on the historical data
 
         Args:
@@ -30,6 +30,11 @@ class RSI(IndicatorUtils):
         """
 
         dataframe = self.convert_to_dataframe(historical_data)
+
+        if lrsi_filter and 'gamma' in lrsi_filter:
+            lrsi = LRSI()
+            dataframe['lrsi'] = dataframe.close.apply(lambda x: lrsi.apply_filter(x, lrsi_filter['gamma']) )
+
         rsi_values = abstract.RSI(dataframe, period_count).to_frame()
         rsi_values.dropna(how='all', inplace=True)
         rsi_values.rename(columns={rsi_values.columns[0]: 'rsi'}, inplace=True)
@@ -38,4 +43,11 @@ class RSI(IndicatorUtils):
             rsi_values['is_hot'] = rsi_values.rsi.apply(lambda x: x > 20 and x < hot_thresh)
             rsi_values['is_cold'] = rsi_values[signal[0]] > cold_thresh
 
+            if lrsi_filter and 'lower_values' in lrsi_filter:
+                lower_min = lrsi_filter['lower_values']['min']
+                lower_max = lrsi_filter['lower_values']['max']
+
+                idx = dataframe['lrsi'].apply(lambda x: x < lower_min or x > lower_max)
+                rsi_values.loc[ idx & (rsi_values['is_hot'] == True), 'is_hot' ] = False
+            
         return rsi_values
