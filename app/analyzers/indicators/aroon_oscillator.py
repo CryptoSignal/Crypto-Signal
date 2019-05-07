29fe676ad16d431dd116c9f3bab758ac4c79f125
ikwtif <dieter.debruyne@gmail.com>
2019-05-07 22:13:45 +0200

    create class

---
+++ app/analyzers/indicators/aroon_oscillator.py
@@ -0,0 +1,16 @@
+
+"""
+Aroon Oscillator
+"""
+
+import pandas
+import numpy
+
+from analyzers.utils import IndicatorUtils
+
+
+class Aroon_oscillator(IndicatorUtils):
+    def analyze(self, historical_data, signal=["adx"], period_count=14, hot_thresh=None, cold_thresh=None):
+        """
+        """
+        pass
