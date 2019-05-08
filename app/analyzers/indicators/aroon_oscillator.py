47fce13c531fc1ccf43cce8edbfc1ba3da83e676
ikwtif <dieter.debruyne@gmail.com>
2019-05-08 21:13:48 +0200

    completing analysis

--- app/analyzers/indicators/aroon_oscillator.py
+++ app/analyzers/indicators/aroon_oscillator.py
@@ -10,7 +10,60 @@ from analyzers.utils import IndicatorUtils
 
 
 class Aroon_oscillator(IndicatorUtils):
-    def analyze(self, historical_data, signal=["adx"], period_count=14, hot_thresh=None, cold_thresh=None):
+    def analyze(self, historical_data, signal=["aroon"], sma_period=50, period_count=25, hot_thresh=None, cold_thresh=None):
+        """Performs an aroon oscillator analysis on the historical data
+
+        Args:
+            historical_data (list): A matrix of historical OHCLV data.
+            signal (list, optional): Defaults to aroon. The indicator
+                line to check hot/cold against.
+            period_count (int, optional): Defaults to 25.
+            sma_count (int, optional): Defaults to 50 (used of hot/cold check)
+            hot_thresh (float, optional): Defaults to None. The threshold at which this might be
+                good to purchase.
+            cold_thresh (float, optional): Defaults to None. The threshold at which this might be
+                good to sell.
+
+        Returns:
+            pandas.DataFrame: A dataframe containing the indicators and hot/cold values.
         """
-        """
-        pass
+        dataframe = self.convert_to_dataframe(historical_data)
+
+        aroon_columns = {
+            'aroon': [numpy.nan] * dataframe.index.shape[0],
+            'aroon_up': [numpy.nan] * dataframe.index.shape[0],
+            'aroon_down': [numpy.nan] * dataframe.index.shape[0]
+        }
+
+        aroon_values = pandas.DataFrame(aroon_columns,
+                                      index=dataframe.index
+                                      )
+
+        for index in range(0, dataframe.shape[0]-24):
+            id = dataframe.shape[0]-index
+            id_period = id - period_count
+            high_date = dataframe['high'].iloc[id_period:id].idxmax()
+            low_date = dataframe['low'].iloc[id_period:id].idxmin()
+
+            periods_since_high = id - dataframe.index.get_loc(high_date) - 1
+            periods_since_low = id - dataframe.index.get_loc(low_date) - 1
+            aroon_values['aroon_up'][id-1] = 100 * ((25 - periods_since_high) / 25)
+
+            aroon_values['aroon_down'][id-1] = 100 * ((25 - periods_since_low) / 25)
+
+        aroon_values['aroon'] = aroon_values['aroon_up'] - aroon_values['aroon_down']
+
+        aroon_values['is_hot'] = False
+        aroon_values['is_cold'] = False
+
+        aroon_values['sma_volume'] = df.dataframe.rolling(sma_period).mean()
+        aroon = aroon_values['aroon']
+        volume = dataframe[volume].iloc[-1]
+        volume_sma = aroon_values['sma_volume'].iloc[-1]
+
+        if aroon > hot_thresh and volume > volume_sma:
+            aroon_values['is_hot'].iloc[-1] = True
+        elif aroon <= cold_thresh and volume < volume_sma:
+            aroon_values['is_cold'].iloc[-1] = True
+
+        return aroon_values
