1cef76cbe83e415e8a0f95a4db6e11cfc788d3da
laliux <eduardo@bbfour.com>
2020-07-08 22:54:14 -0500

    Renamed vars for ma_crossover indicator

--- app/analyzers/indicators/ma_crossover.py
+++ app/analyzers/indicators/ma_crossover.py
@@ -34,15 +34,15 @@ class MACrossover(IndicatorUtils):
             ma_slow_values = abstract.SMA(dataframe, ma_slow)
 
         ma_crossover = pandas.concat([dataframe, ma_fast_values, ma_slow_values], axis=1)
-        ma_crossover.rename(columns={0: 'ma_value_one', 1: 'ma_value_two'}, inplace=True)
+        ma_crossover.rename(columns={0: 'fast_values', 1: 'slow_values'}, inplace=True)
 
-        o1, o2 = ma_crossover.iloc[-2]['ma_value_one'] , ma_crossover.iloc[-2]['ma_value_two']
-        c1, c2 = ma_crossover.iloc[-1]['ma_value_one'] , ma_crossover.iloc[-1]['ma_value_two']
+        previous_fast, previous_slow = ma_crossover.iloc[-2]['fast_values'] , ma_crossover.iloc[-2]['slow_values']
+        current_fast, current_slow = ma_crossover.iloc[-1]['fast_values'] , ma_crossover.iloc[-1]['slow_values']
 
         ma_crossover['is_hot']  = False
         ma_crossover['is_cold'] = False
 
-        ma_crossover['is_hot'].iloc[-1] = o1 < o2 and c1 > c2
-        ma_crossover['is_cold'].iloc[-1] = o1 > o2 and c1 < c2
+        ma_crossover['is_hot'].iloc[-1] = previous_fast < previous_slow and current_fast > current_slow
+        ma_crossover['is_cold'].iloc[-1] = previous_fast > previous_slow and current_fast < current_slow
 
         return ma_crossover
