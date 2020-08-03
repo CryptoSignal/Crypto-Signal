ac934e0d2fc41d36ce98ba83af5488170a496571
laliux <eduardo@bbfour.com>
2020-08-03 15:08:56 -0500

    Added final version of MACD cross indicator

--- app/analyzers/indicators/macd_cross.py
+++ app/analyzers/indicators/macd_cross.py
@@ -1,10 +1,10 @@
 """ MACD Cross
 """
 
-#from talib import abstract
-import talib
-import pandas
 import math
+
+import pandas
+import talib
 
 from analyzers.utils import IndicatorUtils
 
@@ -25,21 +25,23 @@ class MACDCross(IndicatorUtils):
         """
 
         dataframe = self.convert_to_dataframe(historical_data)
-        
-        macd, macdsignal, macdhist = talib.MACD(dataframe['close'], fastperiod=12, slowperiod=26, signalperiod=9)
 
-        macd_values = pandas.DataFrame([macd, macdsignal]).T.rename(columns={0: "macd", 1: "signal"})
+        macd, macdsignal, macdhist = talib.MACD(
+            dataframe['close'], fastperiod=12, slowperiod=26, signalperiod=9)
+
+        macd_values = pandas.DataFrame([macd, macdsignal]).T.rename(
+            columns={0: "macd", 1: "signal"})
 
         macd_cross = pandas.concat([dataframe, macd_values], axis=1)
         macd_cross.dropna(how='all', inplace=True)
 
-        o1, o2 = macd_cross.iloc[-2]['macd'] , macd_cross.iloc[-2]['signal']
-        c1, c2 = macd_cross.iloc[-1]['macd'] , macd_cross.iloc[-1]['signal']
+        previous_macd, previous_signal = macd_cross.iloc[-2]['macd'], macd_cross.iloc[-2]['signal']
+        current_macd, current_signal = macd_cross.iloc[-1]['macd'], macd_cross.iloc[-1]['signal']
 
-        macd_cross['is_hot']  = False
+        macd_cross['is_hot'] = False
         macd_cross['is_cold'] = False
 
-        macd_cross['is_cold'].iloc[-1] = o1 < o2 and c1 > c2
-        macd_cross['is_hot'].iloc[-1] = o1 > o2 and c1 < c2
+        macd_cross['is_cold'].iloc[-1] = previous_macd > previous_signal and current_macd < current_signal
+        macd_cross['is_hot'].iloc[-1] = previous_macd < previous_signal and current_macd > current_signal
 
         return macd_cross
