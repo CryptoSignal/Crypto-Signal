bc2b5c60174a75e1d7d13e3e04d5079efba1060c
laliux <eduardo@bbfour.com>
2020-12-28 16:42:19 -0600

    Renamed config vars for StochRSI Cross

--- app/analyzers/indicators/stochrsi_cross.py
+++ app/analyzers/indicators/stochrsi_cross.py
@@ -32,17 +32,18 @@ class StochRSICross(IndicatorUtils):
         stochrsi_K = stochrsi.rolling(smooth_k).mean()
         stochrsi_D = stochrsi_K.rolling(smooth_d).mean()
 
-        kd_values = pandas.DataFrame([rsi, stochrsi, stochrsi_K, stochrsi_D]).T.rename(columns={0: "rsi", 1: "stoch_rsi", 2: "k", 3: "d"}).copy()
+        kd_values = pandas.DataFrame([rsi, stochrsi, stochrsi_K, stochrsi_D]).T.rename(
+            columns={0: "rsi", 1: "stoch_rsi", 2: "smooth_k", 3: "smooth_d"}).copy()
 
         kd_values['stoch_rsi'] = kd_values['stoch_rsi'].multiply(100)
-        kd_values['k'] = kd_values['k'].multiply(100)
-        kd_values['d'] = kd_values['d'].multiply(100)
+        kd_values['smooth_k'] = kd_values['smooth_k'].multiply(100)
+        kd_values['smooth_d'] = kd_values['smooth_d'].multiply(100)
 
         stoch_cross = pandas.concat([dataframe, kd_values], axis=1)
         stoch_cross.dropna(how='all', inplace=True)
 
-        previous_k, previous_d = stoch_cross.iloc[-2]['k'], stoch_cross.iloc[-2]['d']
-        current_k, current_d = stoch_cross.iloc[-1]['k'], stoch_cross.iloc[-1]['d']
+        previous_k, previous_d = stoch_cross.iloc[-2]['smooth_k'], stoch_cross.iloc[-2]['smooth_d']
+        current_k, current_d = stoch_cross.iloc[-1]['smooth_k'], stoch_cross.iloc[-1]['smooth_d']
 
         stoch_cross['is_hot'] = False
         stoch_cross['is_cold'] = False
